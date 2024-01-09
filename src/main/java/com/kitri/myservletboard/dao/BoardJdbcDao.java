15c94d1d39d94e0942ade221e141643b61698966
jdhert <https://github.com/jdhert/my-servlet-board.git>
2024-01-09 15:36:43 +0900

    JDBC 상세조회/등록/수정/삭제 기능 구현

--- src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
+++ src/main/java/com/kitri/myservletboard/dao/BoardJdbcDao.java
@@ -74,21 +74,107 @@ public class BoardJdbcDao implements BoardDao{
 
     @Override
     public Board getById(Long id) {
-        return null;
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs =null;
+        Board board = null;
+        try {
+            connection = connectDB();
+            String sql = "SELECT * FROM board where id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1 ,id);
+            rs = ps.executeQuery();
+
+            while(rs.next()) {
+                Long id2 = rs.getLong("id");
+                String title = rs.getString("title");
+                String content = rs.getString("content");
+                String writer = rs.getString("Writer");
+                LocalDateTime createdAt = rs.getTimestamp("created_at").toLocalDateTime();
+                int viewCount = rs.getInt("view_count");
+                int commentCount = rs.getInt("comment_count");
+
+                board = new Board(id2, title, content, writer, createdAt, viewCount, commentCount);
+            }
+        } catch (Exception e){
+
+        }finally {
+            try {
+                rs.close();
+                ps.close();
+                connection.close();
+            } catch (Exception e){
+                e.printStackTrace();;
+            }
+        }
+        return board;
     }
 
     @Override
     public void save(Board board) {
-
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = connectDB();
+            String sql = "INSERT INTO board (title, content, writer )values (?,?,?)";
+            ps = connection.prepareStatement(sql);
+            ps.setString(1,board.getTitle());
+            ps.setString(2,board.getContent());
+            ps.setString(3,board.getWriter());
+            ps.executeUpdate();
+        } catch (Exception e){
+        }finally {
+            try {
+                ps.close();
+                connection.close();
+            } catch (Exception e){
+                e.printStackTrace();;
+            }
+        }
     }
 
     @Override
     public void update(Board board) {
-
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = connectDB();
+            String sql = "UPDATE board SET title = ?, content = ?, writer = ? where id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setString(1,board.getTitle());
+            ps.setString(2,board.getContent());
+            ps.setString(3,board.getWriter());
+            ps.setLong(4,board.getId());
+            ps.executeUpdate();
+        } catch (Exception e){
+        }finally {
+            try {
+                ps.close();
+                connection.close();
+            } catch (Exception e){
+                e.printStackTrace();;
+            }
+        }
     }
 
     @Override
     public void delete(Board board) {
-
+        Connection connection = null;
+        PreparedStatement ps = null;
+        try {
+            connection = connectDB();
+            String sql = "DELETE FROM board where id = ?";
+            ps = connection.prepareStatement(sql);
+            ps.setLong(1,board.getId());
+            ps.executeUpdate();
+        } catch (Exception e){
+        }finally {
+            try {
+                ps.close();
+                connection.close();
+            } catch (Exception e){
+                e.printStackTrace();;
+            }
+        }
     }
 }
