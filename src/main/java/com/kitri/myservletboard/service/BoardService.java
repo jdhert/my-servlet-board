1d0d4f22222e2c87b49f9b307359542d181ba7f2
jdhert <https://github.com/jdhert/my-servlet-board.git>
2024-01-09 13:12:09 +0900

    JDBC DB연동 및 게시글 리스트 조회 기능 구현

--- src/main/java/com/kitri/myservletboard/service/BoardService.java
+++ src/main/java/com/kitri/myservletboard/service/BoardService.java
@@ -1,6 +1,7 @@
 package com.kitri.myservletboard.service;
 
 import com.kitri.myservletboard.dao.BoardDao;
+import com.kitri.myservletboard.dao.BoardJdbcDao;
 import com.kitri.myservletboard.dao.BoardMemoryDao;
 import com.kitri.myservletboard.data.Board;
 
@@ -8,7 +9,9 @@ import java.util.ArrayList;
 
 public class BoardService {
 
-    BoardDao boardDao = BoardMemoryDao.getInstance();
+//    BoardDao boardDao = BoardMemoryDao.getInstance();
+
+    BoardDao boardDao = BoardJdbcDao.getInstance();
     private BoardService(){};
     private static final BoardService instance = new BoardService();
 
